827eb8cd3636040b5b00c458c870ef8718167ccf
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2022-12-13 23:56:46 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Toggle bits given range/toggle-bits-given-range.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int toggleBits(int N , int L , int R) {
+        // code here
+        for(int i=L-1;i<R;i++){
+
+            N= N ^ (1<<i);
+
+        }
+
+        return N;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,L,R;
+        
+        cin>>N>>L>>R;
+
+        Solution ob;
+        cout << ob.toggleBits(N,L,R) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
