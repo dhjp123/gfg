7dba9bc0f7a3dc1b3c76c2e59a282f0da84b6a9c
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2022-12-17 00:37:09 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Break a number/break-a-number.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{   
+public:
+    int waysToBreakNumber(long long int n){
+        // code here 
+        //if(n==1)return 3;
+        long long int mod=1e9+7;
+        return(((n+1)*(n+2))%mod/2)%mod;
+       
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.waysToBreakNumber(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
