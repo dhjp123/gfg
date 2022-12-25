2e593c700c24caac082795ebaf9744fca4a11d6c
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2022-12-25 18:34:25 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Number of distinct subsequences/number-of-distinct-subsequences.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:	
+	int distinctSubsequences(string s)
+	{
+	    // Your code goes here
+	    int n=s.length();
+	    const int mod=1e9+7;
+	    vector<int>dp(n+1,0);
+	    dp[0]=1;
+	    unordered_map<char,int>mp;
+	    for(int i=1;i<=n;i++){
+	        char ch=s[i-1];
+	        dp[i]=2*dp[i-1]%mod;
+	        if(mp.find(ch)!=mp.end());
+	        {
+	            int x=mp[ch];
+	            dp[i]=(dp[i]-dp[x-1]+mod)%mod;
+	        }
+	        mp[ch]=i;
+	    }
+	    return dp[n];
+	    
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+        Solution ob;
+   		cout << ob.distinctSubsequences(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
