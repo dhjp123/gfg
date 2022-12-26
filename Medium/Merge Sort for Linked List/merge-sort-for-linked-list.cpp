2cb369892a76fa241f89ddf879008789faec0526
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2022-12-26 23:27:21 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort for Linked List/merge-sort-for-linked-list.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/* Structure of the linked list node is as
+struct Node 
+{
+    int data;
+    struct Node* next;
+    Node(int x) { data = x;  next = NULL; }
+};
+*/
+
+
+class Solution{
+  public:
+    //Function to sort the given linked list using Merge Sort.
+    Node* mergeSort(Node* head) {
+        // your code here
+        Node * temp = head;
+        vector<int> res;
+        
+        while(temp != NULL)
+        {
+            res.push_back(temp->data);
+            temp = temp->next;
+        }
+        sort(res.begin(), res.end());
+        
+        temp = head;
+        int i=0;
+        while(temp != NULL)
+        {
+            temp->data = res[i];
+            i++;
+            temp = temp->next;
+        }
+        
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+void printList(Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+void push(struct Node** head_ref, int new_data) {
+    Node* new_node = new Node(new_data);
+
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+int main() {
+    long test;
+    cin >> test;
+    while (test--) {
+        struct Node* a = NULL;
+        long n, tmp;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> tmp;
+            push(&a, tmp);
+        }
+        Solution obj;
+        a = obj.mergeSort(a);
+        printList(a);
+    }
+    return 0;
+}
+// } Driver Code Ends
