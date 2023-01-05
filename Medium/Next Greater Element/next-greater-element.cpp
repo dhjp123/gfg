2f59869596c11fb3e7cce2302f284fa13b864c89
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-01-05 18:07:24 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Next Greater Element/next-greater-element.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User Function Template for C++ solution
+
+class Solution {
+  public:
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        stack<long long >s;
+        s.push(-1);
+        vector<long long >v;
+        for(int i=n-1;i>=0;i--){
+            if(s.size()==0){
+                 v.push_back(-1);
+            }
+            else if(s.size()>0 && s.top()>arr[i]){
+                v.push_back(s.top());
+            }
+            else if(s.size()>0 && s.top()<=arr[i]){
+                while(s.size()>0 && s.top()<=arr[i]){
+                    s.pop();
+                }
+                 if(s.size()==0)
+                 {
+                   v.push_back(-1);
+                 }else{
+                     v.push_back(s.top());
+                 }
+            }
+            s.push(arr[i]);
+                
+                    
+                
+        }
+        reverse(v.begin(),v.end());
+        
+        return v;
+            
+            
+            
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        
+        vector <long long> res = ob.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
