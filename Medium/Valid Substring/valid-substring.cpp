648e6b414c31c40fe5aadec0d4445d6b58038490
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-01-05 15:03:05 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Valid Substring/valid-substring.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for c++
+class Solution {
+  public:
+    int findMaxLen(string s) {
+        // code here
+        stack<int>st;
+        st.push(-1);
+        int n= s.length();
+        int res=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(!st.empty()){
+                    res=max(res,i-st.top());
+                }
+                else{
+                    st.push(i);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S; 
+        Solution ob;
+        cout << ob.findMaxLen(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
