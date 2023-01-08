9bca825db53bd4318f0107d325ea1e193110c6f1
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-01-09 00:10:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Last modified ball/last-modified-ball.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int solve(int n, vector<int> a) {
+        // code here
+        int cnt=n;
+        while(a[n-1]==9){
+            n--;
+            cnt--;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        vector<int> A(N);
+
+        for (int i = 0; i < N; i++) cin >> A[i];
+
+        Solution ob;
+        cout << ob.solve(N, A) << "\n";
+    }
+    return 0;
+}
+    
+// } Driver Code Ends
