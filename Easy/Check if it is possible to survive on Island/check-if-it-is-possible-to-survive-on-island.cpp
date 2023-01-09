1e22bf932419b34ee6d27fab974e17c2d4086ef9
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-01-09 18:40:48 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Check if it is possible to survive on Island/check-if-it-is-possible-to-survive-on-island.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int minimumDays(int s, int n, int m){
+        // code here
+        if((n<m && s<7)||((n*6<m*7) && (s>6))){
+            return -1;
+            
+        }
+        return (s*m+n-1)/n;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int S, N, M;
+        cin>> S >> N >> M;
+        
+        Solution ob;
+        cout<<ob.minimumDays(S, N, M)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
