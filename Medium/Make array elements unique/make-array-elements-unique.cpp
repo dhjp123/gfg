9da509f7bb76451802b49bb02fb6bdc9a7953afb
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-01-11 01:02:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Make array elements unique/make-array-elements-unique.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long int minIncrements(vector<int> arr, int N) {
+        // Code here
+        int n =arr.size();
+        long long cnt=0;
+        sort(arr.begin(),arr.end());
+        for(int i=1;i<n;i++){
+            while(arr[i]<=arr[i-1]){
+                arr[i]++;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.minIncrements(arr, N) << endl;
+    }
+}
+// } Driver Code Ends
