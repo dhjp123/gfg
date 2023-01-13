df1ced81ecbcf33eb115e6f304ef700fdcb0f2e4
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-01-13 21:13:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* sortedMerge(struct Node* a, struct Node* b);
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *n)
+{
+    while (n!=NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        int data;
+        cin>>data;
+        struct Node *head1 = new Node(data);
+        struct Node *tail1 = head1;
+        for (int i = 1; i < n; ++i)
+        {
+            cin>>data;
+            tail1->next = new Node(data);
+            tail1 = tail1->next;
+        }
+
+        cin>>data;
+        struct Node *head2 = new Node(data);
+        struct Node *tail2 = head2;
+        for(int i=1; i<m; i++)
+        {
+            cin>>data;
+            tail2->next = new Node(data);
+            tail2 = tail2->next;
+        }
+
+        Node *head = sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+ 
+
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+  
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+//Function to merge two sorted linked list.
+Node* sortedMerge(Node* h1, Node* h2)  
+{ 
+    if(h1==nullptr){
+        return h2;
+    }
+    if(h2==nullptr){
+        return h1;
+    }
+    if(h1->data>h2->data){
+        std::swap(h1,h2);
+    }
+    Node*res= h1;
+    while(h1!=nullptr && h2!=nullptr){
+        Node* temp= nullptr;
+        while(h1!=nullptr && h1->data<=h2->data){
+            temp=h1;
+            h1=h1->next;
+            
+        }
+        temp->next=h2;
+        std::swap(h1,h2);
+    }
+    return res;
+    // code here
+}  
