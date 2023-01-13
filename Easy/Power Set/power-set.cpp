faf29f19c31c8a3839412fd9f71918118fcbdf42
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-01-13 12:01:37 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Power Set/power-set.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+		vector<string> AllPossibleStrings(string s){
+		    // Code here
+		    int n = s.length();
+          	vector<string>ans;
+	       for (int num = 0; num < (1 << n); num++) {
+	        	string sub = "";
+	         	for (int i = 0; i < n; i++)
+	         	{
+			//check if the ith bit is set or not
+			       if (num & (1 << i)) {
+			     	   sub += s[i];
+			       }
+	         	}
+		       if (sub.length() > 0)
+		       {
+			      ans.push_back(sub);
+	        	}
+        	}
+         	sort(ans.begin(), ans.end());
+	        return ans;
+
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string s;
+		cin >> s;
+		Solution ob;
+		vector<string> res = ob.AllPossibleStrings(s);
+		for(auto i : res)
+			cout << i <<" ";
+		cout << "\n";
+
+	}
+	return 0;
+}
+// } Driver Code Ends
