b47b18d8627b86f425769f763228cf3c007441b1
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-01-14 23:35:15 +0530

    Added Solution - GfG to GitHub

---
+++ School/Lower case to upper case/lower-case-to-upper-case.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+string to_upper(string in);
+ 
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string str;
+		cin>>str;
+		cout << to_upper(str)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+
+string to_upper(string str){
+    for(int i=0;i<str.length();i++){
+        str[i]=toupper(str[i]);
+        
+    }
+    return str;
+    //code
+    
+}
