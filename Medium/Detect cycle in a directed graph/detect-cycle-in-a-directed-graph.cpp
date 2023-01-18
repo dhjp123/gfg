8db8a71e2dc32627f21fb1de65ddbe8f746fbea9
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-01-18 20:15:16 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
+++ Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
@@ -6,40 +6,38 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    bool dfsrec(vector<int>adj[], int s, bool visited[], bool recst[]){
-        visited[s]=true;
-        recst[s]=true;
-        for(int u:adj[s]){
-            if(visited[u]==false && dfsrec(adj,u,visited,recst)== true){
-                return true;
-            }
-            else if(recst[u]==true){
-                return true;
-            }
-           // recst[s]=false;
-            
-        }
-         recst[s]=false;
-         return false;
-    }
     bool isCyclic(int v, vector<int> adj[]) {
         // code here
-        bool visited[v];
-        for(int i=0;i<v;i++){
-            visited[i]=false;
-        }
-        bool recst[v];
-        for(int i=0;i<v;i++){
-            recst[i]=false;
-        }
-        for(int i=0;i<v;i++){
-            if(visited[i]==false){
-                if(dfsrec(adj,i,visited,recst)==true){
-                    return true;
-                }
-            }
-        }
-        return false;
+         int indgree[v]={0};
+	    for(int i=0;i<v;i++){
+	        for(auto it:adj[i]){
+	            indgree[it]++;
+	        }
+	    }
+	    queue<int>q;
+	    for(int i=0;i<v;i++){
+	        if(indgree[i]==0){
+	            q.push(i);
+	        }
+	    }
+	    int cnt=0;
+	    vector<int>ans;
+	    while(!q.empty()){
+	        int x= q.front();
+	        q.pop();
+	        cnt++;
+	        //ans.push_back(x);
+	        for(auto it:adj[x]){
+	            indgree[it]--;
+	            if(indgree[it]==0){
+	                q.push(it);
+	            }
+	        }
+	    }
+	    if(cnt==v){
+	        return false;
+	    }
+	    return true;
     }
 };
 
