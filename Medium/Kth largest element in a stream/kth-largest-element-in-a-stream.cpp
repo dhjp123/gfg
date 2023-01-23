427256bc26d0973d2c7bb64d7701bed4f45650a0
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-01-23 16:16:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Kth largest element in a stream/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> kthLargest(int k, int arr[], int n) {
+        vector<int>ans;
+       priority_queue<int,vector<int>,greater<int>>pq;
+       for(int i=0;i<n;i++)
+       {
+           pq.push(arr[i]);
+           if(pq.size()<k)
+           ans.push_back(-1);
+           else if(pq.size()==k)ans.push_back(pq.top());
+           else {
+               pq.pop();
+               ans.push_back(pq.top());
+           }
+           
+       }
+       return ans;
+       
+       
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int k,n;
+        cin>>k>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        vector<int> v = ob.kthLargest(k,arr,n);
+        for(int i : v)
+            cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
