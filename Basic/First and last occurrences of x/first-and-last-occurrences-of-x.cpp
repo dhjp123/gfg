8cd87ad5ff50dadc4842d0701073330d12cbad7a
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-01-28 01:27:49 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/First and last occurrences of x/first-and-last-occurrences-of-x.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+vector<int> find(int arr[], int n , int x )
+{
+    // code here
+    vector<int>v(2,-1);
+    for(int i=0;i<n;i++){
+        if(arr[i]==x){
+           // v.push_back(i);
+            v[0]=i;
+            break;
+        }
+    }
+    for(int i=n-1;i>=0;i--){
+        if(arr[i]==x){
+            //v.push_back(i);
+            v[1]=i;
+            break;
+        }
+    }
+    
+    
+    return v;
+    
+    
+    
+}
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        ans=find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
