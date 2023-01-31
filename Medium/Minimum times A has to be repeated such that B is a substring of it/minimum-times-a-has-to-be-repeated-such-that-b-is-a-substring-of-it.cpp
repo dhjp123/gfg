eb9066a5481e9f61a6a37dbbeb6782180eaab8a8
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-01-31 16:54:53 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum times A has to be repeated such that B is a substring of it/minimum-times-a-has-to-be-repeated-such-that-b-is-a-substring-of-it.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int minRepeats(string a, string b) {
+        int cnt=1;
+        string s=a;
+        while(a.size()<b.size()){
+            a+=s;
+            cnt++;
+        }
+        if(a.find(b)!=-1){
+            return cnt;
+        }
+        a+=s;
+        cnt++;
+        if(a.find(b)!=-1){
+            return cnt;
+        }
+        return -1;
+       
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ",&t);
+    while (t--) {
+        string A,B;
+        getline(cin,A);
+        getline(cin,B);
+
+        Solution ob;
+        cout << ob.minRepeats(A,B) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
