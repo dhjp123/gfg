792a169e6f0bfabe0919e99a4826d1240e2b463b
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-02-02 16:15:50 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Plus One/plus-one.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<int> increment(vector<int> arr ,int n) {
+        // code here
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]==9){
+                arr[i]=0;
+            }
+            else{
+                arr[i]=arr[i]+1;
+                return arr;
+            }
+        }
+        arr.insert(arr.begin(),1);
+        return arr;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        char c;
+        
+        cin>>N;
+        vector<int> arr(N);
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        vector<int> res = ob.increment(arr,N);
+        for(int i: res)
+            cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
