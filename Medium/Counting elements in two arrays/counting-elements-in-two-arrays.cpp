f28a639b5e2a293108f7a48e0c576230d1e99201
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-02-02 13:29:56 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Counting elements in two arrays/counting-elements-in-two-arrays.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// C++ implementation of For each element in 1st 
+// array count elements less than or equal to it
+// in 2nd array
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+
+// } Driver Code Ends
+// function to count for each element in 1st array,
+// elements less than or equal to it in 2nd array
+class Solution{
+  public:
+    vector<int> countEleLessThanOrEqual(int arr1[], int arr2[], 
+                                 int m, int n)
+    {
+          vector<int>ans(m);
+
+         int pre[100001]={};
+
+         for(int i=0;i<n;i++){
+
+             pre[arr2[i]]++;
+
+         }
+
+         for(int i=1;i<100001;i++){
+
+             pre[i]+=pre[i-1];
+
+         }
+
+         int cnt=0;
+
+         for(int i=0;i<m;i++){
+
+              ans[i]=pre[arr1[i]]; }
+
+         return ans;
+    //Your code goes here
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m,n;
+        cin>>m>>n;
+        int arr1[m],arr2[n];
+        for(int i=0;i<m;i++)
+        cin>>arr1[i];
+        for(int j=0;j<n;j++)
+        cin>>arr2[j];
+        Solution obj;
+        vector <int> res = obj.countEleLessThanOrEqual(arr1, arr2, m, n);
+        for (int i = 0; i < res.size (); i++) cout << res[i] << " ";
+        
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
