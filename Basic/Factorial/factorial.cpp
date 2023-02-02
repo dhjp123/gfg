fb2630cc1a728e2cfb5380c7865a7b19f34e9ff8
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-02-02 11:58:52 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Factorial/factorial.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    long long int factorial(int n){
+        if(n==1||n==0){
+            return 1;
+        }
+        return factorial(n-1)*n;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
