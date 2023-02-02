c498ed5bc945448bc5a14be6bbf4c44eb580d4ac
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-02-02 12:53:37 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count the Zeros/count-the-zeros.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int countZeroes(int arr[], int n) {
+        // code here
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
+        }
+        for(auto i:mp){
+            if(i.first==0){
+                ans.push_back(i.second);
+            }
+        }
+        return ans[0];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.countZeroes(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
