d5facbc14b026b0b85aaff25bea23a415591c18c
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-02-08 01:24:51 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find Prime numbers in a range/find-prime-numbers-in-a-range.cpp
+++ Medium/Find Prime numbers in a range/find-prime-numbers-in-a-range.cpp
@@ -9,18 +9,18 @@ using namespace std;
 
 class Solution {
   public:
-  bool pm(int n){
+  int  pm(int n){
        if(n<=1){
-              return false;
+              return 0;
           }
       for(int i=2;i<=sqrt(n);i++){
          
          
           if(n%i==0){
-              return false;
+              return 0;
           }
       }
-      return true;
+      return 1;
   }
     vector<int> primeRange(int m, int n) {
         vector<int>ans;
