6ff2347e60c2c1afe06744091c858536e037c253
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-02-12 22:09:39 +0530

    Updated Solution - GfG to GitHub

--- Medium/Count ways to N'th Stair(Order does not matter)/count-ways-to-nth-stair-order-does-not-matter.cpp
+++ Medium/Count ways to N'th Stair(Order does not matter)/count-ways-to-nth-stair-order-does-not-matter.cpp
@@ -3,29 +3,42 @@
 using namespace std;
 
 // } Driver Code Ends
-class Solution{
-	public:
-		int nthStair(int n)
-		{
-		    
-		    
-		    return n/2+1;
-		    
-		    
-		}
+
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int n)
+    {
+        
+        int dp[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=1+min(dp[i-1],dp[i-2]);
+        }
+        return dp[n];
+    }
 };
 
 //{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		Solution ob;
-		int ans  = ob.nthStair(n);
-		cout << ans <<"\n";
-	}
-	return 0;
+int main()
+{
+    //taking count of testcases
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
 }
 // } Driver Code Ends
