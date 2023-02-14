0970b6601be839b003126887d43e2393053bc5d2
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-02-14 20:41:16 +0530

    Updated Solution - GfG to GitHub

--- Medium/Help Classmates/help-classmates.cpp
+++ Medium/Help Classmates/help-classmates.cpp
@@ -13,37 +13,40 @@ class Solution{
     public:
     vector<int> help_classmate(vector<int> arr, int n) 
     { 
-        // Your code goes here
+        
+        
+       
+       vector<int >right;
         stack<int>s;
-        vector<int>v;
-      //  for(int i= n-1;i>=0;i--){
-        //    s.push(arr[i]);
-        //}
+        int p=-1;
         for(int i=n-1;i>=0;i--){
             if(s.size()==0){
-                v.push_back(-1);
+                right.push_back(-1);
             }
             else if(s.size()>0 && s.top()<arr[i]){
-                v.push_back(s.top());
-                
+                right.push_back(s.top());
             }
             else if(s.size()>0 && s.top()>=arr[i]){
                 while(s.size()>0 && s.top()>=arr[i]){
                     s.pop();
                 }
-                if(s.size()==0){
-                    v.push_back(-1);
-                }
-                else{
-                    v.push_back(s.top());
-                }
-                
+                 if(s.size()==0)
+                 {
+                   right.push_back(-1);
+                 }else{
+                     right.push_back(s.top());
+                 }
             }
-            
             s.push(arr[i]);
+                
+                    
+                
         }
-        reverse(v.begin(),v.end());
-        return v;
+        reverse(right.begin(),right.end());
+        
+        return right;
+        
+        // Your code goes here
     } 
 };
 
