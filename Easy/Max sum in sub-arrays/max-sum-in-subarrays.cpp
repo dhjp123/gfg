6a2e208654e3961985b0f913f64271607cc3ccdc
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-02-25 15:58:48 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Max sum in sub-arrays/max-sum-in-subarrays.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+        long long pairWithMaxSum(long long arr[], long long n)
+    {
+        long long int maxi=INT_MIN;
+        
+        
+        for(int i=0;i<n-1;i++){
+            maxi=max(maxi,(arr[i]+arr[i+1]));
+            
+        }
+        return maxi;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() 
+{
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		ll a[n];
+	    for(ll i=0;i<n;i++)
+		    cin>>a[i];
+		Solution ob;
+	    cout<<ob.pairWithMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+
+
+// } Driver Code Ends
