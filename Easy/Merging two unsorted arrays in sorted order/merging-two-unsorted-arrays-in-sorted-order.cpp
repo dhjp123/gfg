7eb856feea12b86fb0f70ce46760dcda1c2e20b8
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-03-01 23:45:54 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Merging two unsorted arrays in sorted order/merging-two-unsorted-arrays-in-sorted-order.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// Your code goes hereclass Solution{
+	
+class Solution{
+	public:
+	void sortedMerge(int a[], int b[], int res[],int n, int m)
+	{
+	    int ans[n+m];
+	    int i=0;
+	    for(i=0;i<n;i++){
+	        res[i]=a[i];
+	        
+	    }
+	    for(int j=0;j<m;j++){
+	        res[i]=b[j];
+	        i++;
+	    }
+	    
+	    sort(res,res+(n+m));
+	   
+	   
+	    
+	    
+	    
+	    
+	  
+	}
+};
+	  
+
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n, m;
+	    cin >> n >> m;
+	    int a[n], b[m];
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	    }
+
+	    for(int i = 0;i<m;i++){
+	        cin >> b[i];
+	    }
+	    
+	    int res[n+m];
+
+      
+        Solution ob;
+        ob.sortedMerge(a,b,res,n,m);
+
+        for(int i = 0;i<n+m;i++){
+	        cout << res[i] << " ";
+	    }
+	   
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
