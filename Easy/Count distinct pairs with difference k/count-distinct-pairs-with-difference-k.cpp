6f96185df30e732465221a2b22dcc9a196bf3634
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-03-02 00:06:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count distinct pairs with difference k/count-distinct-pairs-with-difference-k.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	int TotalPairs(vector<int>nums, int k){
+	    
+	     sort(nums.begin(),nums.end());
+        int ans=0,i=0,j=1;
+        for(i,j;i<nums.size() and j<nums.size();)
+        {
+            if(i==j or nums[j]-nums[i]<k)
+                j++;
+            else 
+            {
+                if(nums[j]-nums[i]==k)
+                {
+                    ans++;
+                    j++;
+                    for(;j<nums.size();j++)
+                        if(nums[j]!=nums[j-1])
+                            break;
+                    if(j==nums.size())
+                    return ans;
+                    j--;                  
+                }
+                i++;
+                while(i<j and nums[i]==nums[i-1])
+                    i++;
+            }
+        }
+        return ans;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, k;
+		cin >> n >> k;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)
+			cin >> nums[i];
+		Solution ob;
+		int ans = ob.TotalPairs(nums, k);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
