034603764fd5cdc48a6a737199552ba5615f122b
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-03-21 01:46:01 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Taxi Booking/taxi-booking.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int minimumTime(int n,int c,vector<int> &pos,vector<int> &t){
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++){
+            int r=abs(c-pos[i])*t[i];
+            mini=min(mini,r);
+            
+        }
+        return mini;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N,cur;
+        cin>>N>>cur;
+        vector<int> pos(N),time(N);
+        for(int i=0;i<N;i++){
+            cin>>pos[i];
+        }
+        for(int i=0;i<N;i++){
+            cin>>time[i];
+        }
+        Solution ob;
+        cout<<ob.minimumTime(N,cur,pos,time)<<endl;
+    }
+}
+// } Driver Code Ends
