6dcd6d8aaa8222ca2b442440587578a954a2968f
dhjp123 <100591725+dhjp123@users.noreply.github.com>
2023-04-02 01:00:49 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Nth Natural Number/nth-natural-number.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution{
+	public:
+    	long long findNth(long long N)
+    {
+         string ans="";
+
+        while(N){
+
+            ans=to_string(N%9)+ans;
+
+            N/=9;
+
+        }
+
+        return stoll(ans);
+        // code here.
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n , ans;
+		cin>>n;
+		Solution obj;
+		ans = obj.findNth(n);
+		cout<<ans<<endl;
+	}
+}
+
+
+// } Driver Code Ends
